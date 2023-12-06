0d0d8ccc9b831e28bcf34a027c35c7aa2396ea20
SebastianPineda2023 <Sebastian.Pineda@bloomy.com>
2023-12-06 14:55:22 -0500

    stubbing argparse and adding my input

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use argparse::{ArgumentParser, Store};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() -> Result<(), std::io::Error> {
+    let mut file_name: String = "".to_string();
+
+    {
+        let mut parser: ArgumentParser<'_> = ArgumentParser::new();
+        parser.set_description("Parse a text input for Day 2 of Advent of Code");
+        parser
+            .refer(&mut file_name)
+            .add_option(&["-f", "--file"], Store, "Text File Name");
+        parser.parse_args_or_exit();
+    }
+
+    let file_name: String = file_name;
+
+    if file_name.is_empty() {
+        println!("Use: main.exe <TEXT_FILE>");
+        std::process::exit(1);
+    }
+
+    if !file_name.ends_with(".txt") {
+        println!("<TEXT_FILE> must end with .txt!");
+        std::process::exit(1);
+    }
+
+    println!("File to read: {}", file_name);
+
+    let file: File = File::open(file_name)?;
+    let file_reader: BufReader<&File> = BufReader::new(&file);
+
+    Ok(())
 }

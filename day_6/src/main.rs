05f4e0afc1c8fb3a8e05d32792abc63a85a458bb
SebastianPineda2023 <Sebastian.Pineda@bloomy.com>
2023-12-08 17:55:37 -0500

    def not the most efficient; day 6 done

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -1,3 +1,57 @@
+use argparse::{ArgumentParser, Store};
+use std::io::{BufRead, BufReader};
+
+#[derive(Debug)]
+struct Race {
+    time: usize,
+    distance: usize,
+}
+
+impl Race {
+    pub fn new(time_param: usize, distance_param: usize) -> Race {
+        Race {
+            time: time_param,
+            distance: distance_param,
+        }
+    }
+
+    pub fn possible_wins(&self) -> usize {
+        let mut wins: usize = 0;
+        for seconds_held in 1..self.time {
+            if seconds_held * (self.time - seconds_held) > self.distance {
+                wins += 1;
+            }
+        }
+        wins
+    }
+}
+
+fn add_race(race_array: &mut Vec<Race>, time: &str, distance: &str) -> Result<(), std::io::Error> {
+    let time_param: Result<usize, std::num::ParseIntError> = time.parse();
+    let time_param: usize = match time_param {
+        Ok(x) => x,
+        Err(_) => {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::Interrupted,
+                "Number not parsed correctly",
+            ))
+        }
+    };
+
+    let distance_param: Result<usize, std::num::ParseIntError> = distance.parse();
+    let distance_param: usize = match distance_param {
+        Ok(x) => x,
+        Err(_) => {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::Interrupted,
+                "Number not parsed correctly",
+            ))
+        }
+    };
+    race_array.push(Race::new(time_param, distance_param));
+    Ok(())
+}
+
 fn main() -> Result<(), std::io::Error> {
     let mut file_name: String = "".to_string();
 
@@ -24,5 +78,49 @@ fn main() -> Result<(), std::io::Error> {
 
     println!("File to read: {}", file_name);
 
-    let file: std:fs::File = std::fs::File::open(file_name)?;
+    let file: std::fs::File = std::fs::File::open(file_name)?;
+    let file_reader: BufReader<&std::fs::File> = BufReader::new(&file);
+    let mut races: Vec<Race> = Vec::new();
+    let mut times: Vec<String> = Vec::new();
+    let mut distances: Vec<String> = Vec::new();
+
+    for line in file_reader.lines() {
+        match line {
+            Ok(x) => {
+                let tokens = x.split_whitespace().collect::<Vec<&str>>();
+                match tokens[0] {
+                    "Time:" => {
+                        for token in tokens.iter().skip(1) {
+                            times.push(token.to_string());
+                        }
+                    }
+                    "Distance:" => {
+                        for token in tokens.iter().skip(1) {
+                            distances.push(token.to_string());
+                        }
+                    }
+                    &_ => continue,
+                }
+            }
+            Err(_) => continue,
+        }
+    }
+
+    println!("Times: {:?}\nDistances: {:?}", times, distances);
+
+    for index in 0..times.len() {
+        add_race(&mut races, &times[index], &distances[index])?;
+    }
+
+    println!("Races: {:?}", races);
+
+    let mut wins: Vec<usize> = Vec::new();
+
+    for race in races {
+        wins.push(race.possible_wins());
+    }
+
+    println!("Wins: {}", wins.iter().product::<usize>());
+
+    Ok(())
 }

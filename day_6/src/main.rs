53294b4a7477169bdb94b34f29bcdad26bbcc8bb
SebastianPineda2023 <Sebastian.Pineda@bloomy.com>
2023-12-08 17:06:10 -0500

    start day 6

---
+++ day_6/src/main.rs
@@ -0,0 +1,28 @@
+fn main() -> Result<(), std::io::Error> {
+    let mut file_name: String = "".to_string();
+
+    {
+        let mut parser: ArgumentParser<'_> = ArgumentParser::new();
+        parser.set_description("Parse a text input for Day 2 of Advent of Code");
+        parser
+            .refer(&mut file_name)
+            .add_option(&["-f", "--file"], Store, "Text File Name");
+        parser.parse_args_or_exit();
+    }
+
+    let file_name: String = file_name;
+
+    if file_name.is_empty() {
+        println!("Use: main.exe <TEXT_FILE>");
+        std::process::exit(1);
+    }
+
+    if !file_name.ends_with(".txt") {
+        println!("<TEXT_FILE> must end with .txt!");
+        std::process::exit(1);
+    }
+
+    println!("File to read: {}", file_name);
+
+    let file: std:fs::File = std::fs::File::open(file_name)?;
+}
